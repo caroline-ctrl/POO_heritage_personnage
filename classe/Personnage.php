1202f00b738ee1d10dcd18b7f7d1f4653eb4786e
caroline-ctrl <duon.caroline@gmail.com>
2020-02-19 09:38:46 +0100

    initial commit et creationde la classe personnage et getter

---
+++ classe/Personnage.php
@@ -0,0 +1,24 @@
+<?php
+class Personnage{
+    private $nom;
+    private $force;
+    private $santé;
+
+    /*********GETTER*********/
+
+    /**
+     * return $nom
+     * type: string
+     */
+    public function getName(){
+        return $this->name;
+    }
+
+    /**
+     * return $force
+     * typ
+     */
+
+    public function seDeplacer(){}
+    public function parler(){}
+}
